fe77d9890d0c56de81a8699a5404e5c0ac8496d0
unknown <jhfernandez@deakin.edu.au>
2021-07-17 13:54:19 +1000

    ADDED local storage and register device functionality

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-const devices = [];
+const devices = JSON.parse(localStorage.getItem('devices')) || [];
 
-devices.push({ user: "Mary", name: "Mary's iPhone" });
-devices.push({ user: "Alex", name: "Alex's Surface Pro" });
-devices.push({ user: "Mary", name: "Mary's MacBook" });
+
 
 devices.forEach(function(device) {
     $('#devices tbody').append(`
@@ -14,4 +12,15 @@ devices.forEach(function(device) {
     console.log(device.name);
     
   });
+
+  $('#add-device').on('click', function() {
+    const user = $('#user').val();
+    const name = $('#name').val();
+    devices.push({ user: user, name: name });
+    localStorage.setItem('devices',JSON.stringify(devices))
+    console.log(devices);
+    location.href = 'Device_List.html';
+  });
+    
+
   
